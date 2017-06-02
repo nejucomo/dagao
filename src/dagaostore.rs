48df46b4633a9c2411cf7d5584fa78e721ea15f3
Nathan Wilcox <nathan@z.cash>
2017-06-01 18:03:10 -0700

    [No compile] Add the linknode header to the write side to match the read side.

--- src/dagaostore.rs
+++ src/dagaostore.rs
@@ -45,7 +45,7 @@ impl DagaoStore {
 
     pub fn open_linknode_inserter(&self) -> io::Result<LinkNodeInserter> {
         let hins = try!(self.hstore.open_inserter());
-        Ok(LinkNodeInserter::init(hins))
+        LinkNodeInserter::init(hins)
     }
 
     pub fn open_linknode_reader(&self, hash: &Hash) -> io::Result<LinkNodeReader> {
@@ -65,13 +65,24 @@ impl DagaoStore {
 }
 
 
+const LINK_NODE_HEADER: &[u8] = "dagao 0\n".as_bytes();
+
+
 pub struct LinkNodeInserter<'a> {
     hins: HashInserter<'a>,
 }
 
 impl<'a> LinkNodeInserter<'a> {
-    fn init(hins: HashInserter<'a>) -> LinkNodeInserter<'a> {
-        LinkNodeInserter { hins: hins }
+    fn init(hins: HashInserter<'a>) -> io::Result<LinkNodeInserter<'a>> {
+        let n = try!(hins.write(LINK_NODE_HEADER));
+        if n == LINK_NODE_HEADER.len() {
+            Ok(LinkNodeInserter { hins: hins })
+        } else {
+            // FIXME Handle 0 < n < LINK_NODE_HEADER.len().
+            use std::io::{Error, ErrorKind};
+            Err(Error::new(ErrorKind::WriteZero,
+                           "could not write header in single write"))
+        }
     }
 }
 
@@ -82,10 +93,9 @@ pub struct LinkNodeReader {
 
 impl LinkNodeReader {
     fn wrap_file(mut f: fs::file) -> io::Result<LinkNodeReader> {
-        let header = "dagao 0\n".as_bytes();
-        let mut buf = &[0u8; header.len()];
+        let mut buf = &[0u8; LINK_NODE_HEADER.len()];
         let n = try!(f.read(&mut buf));
-        if n == header.len() && header == buf {
+        if n == LINK_NODE_HEADER.len() && buf == LINK_NODE_HEADER {
             Ok(LinkNodeReader { f: f })
         } else {
             use std::io::{Error, ErrorKind};
